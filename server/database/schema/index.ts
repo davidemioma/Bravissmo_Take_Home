9dcb2faeb37c3ed49e1aeb617624c17814089186
david emioma <davidemiomauche@gmail.com>
2025-05-20 13:06:54 +0100

    Setup database schemas.

--- server/database/schema/index.ts
+++ server/database/schema/index.ts
@@ -1,9 +1,74 @@
-import { pgTable, text, uuid, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+  pgEnum,
+  real,
+  integer,
+} from "drizzle-orm/pg-core";
+
+export const ProductType = pgEnum("productType", [
+  "Bras",
+  "Swimwear",
+  "Sports Bras",
+  "Sports Wear",
+  "Nightwear",
+  "Vest Tops",
+  "lingerie",
+]);
 
 export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
+  clerkId: text("clerkId").unique().notNull(),
   name: text("name").notNull(),
   email: text("email").unique().notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const products = pgTable("products", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  images: text("image").array(),
+  type: ProductType("productType").notNull(),
+  color: text("color").notNull(),
+  price: real("price").notNull(),
+  quantity: real("quantity").default(0),
+  sizes: text("sizes").array(),
+  bandSizes: integer("bandSizes").array(),
+  cupSizes: integer("cupSizes").array(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const cartItem = pgTable("cartItem", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: uuid("userId")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  productId: uuid("productId")
+    .references(() => products.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  quantity: real("quantity").default(0),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const reviews = pgTable("reviews", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: uuid("userId")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  productId: uuid("productId")
+    .references(() => products.id, { onDelete: "cascade" })
+    .notNull(),
+  name: text("name").notNull(),
+  comment: text("comment").notNull(),
+  rating: real("rating").default(0),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
---
+++ server/drizzle/0000_tricky_rafael_vega.sql
@@ -0,0 +1,52 @@
+CREATE TYPE "public"."productType" AS ENUM('Bras', 'Swimwear', 'Sports Bras', 'Sports Wear', 'Nightwear', 'Vest Tops', 'lingerie');--> statement-breakpoint
+CREATE TABLE "cartItem" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"userId" uuid NOT NULL,
+	"productId" uuid NOT NULL,
+	"quantity" real DEFAULT 0,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "products" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"image" text[],
+	"productType" "productType" NOT NULL,
+	"color" text NOT NULL,
+	"price" real NOT NULL,
+	"quantity" real DEFAULT 0,
+	"sizes" text[],
+	"bandSizes" integer[],
+	"cupSizes" integer[],
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "reviews" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"userId" uuid NOT NULL,
+	"productId" uuid NOT NULL,
+	"name" text NOT NULL,
+	"comment" text NOT NULL,
+	"rating" real DEFAULT 0,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"clerkId" text NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"image" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_clerkId_unique" UNIQUE("clerkId"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "cartItem" ADD CONSTRAINT "cartItem_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "cartItem" ADD CONSTRAINT "cartItem_productId_products_id_fk" FOREIGN KEY ("productId") REFERENCES "public"."products"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_productId_products_id_fk" FOREIGN KEY ("productId") REFERENCES "public"."products"("id") ON DELETE cascade ON UPDATE no action;
